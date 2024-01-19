7ed631b09753bb99020aae2dc59e6ce0657b40f5
Manash-Christ <139947966+Manash-Christ@users.noreply.github.com>
2024-01-20 01:37:03 +0530

    Add files via upload

---
+++ lab8/javaJDBC/src/main/java/com/mycompany/javajdbc/JavaJDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javajdbc;
+
+/**
+ *
+ * @author lenovo
+ */
+public class JavaJDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
